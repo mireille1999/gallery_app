f7240919a780452c3584bfc7c72078319454c901
mireille1999 <muhawenimana920@daviscollege.com>
2021-04-14 14:21:06 +0200

    updated requirment

--- myProject/settings.py
+++ myProject/settings.py
@@ -90,8 +90,7 @@ if config('MODE')=="dev":
            'NAME': config('DB_NAME'),
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
+          
        }
        
    }
